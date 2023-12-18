77668ecd9d57a7010052bb31e3878724f25cdc8f
Rohith <95613210+RohiV04@users.noreply.github.com>
2023-12-19 00:41:09 +0530

    Add route for root endpoint

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ require("dotenv").config();
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const app = express();
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 app.listen(process.env.PORT || 9000, () => {
   console.log(`Server is running on ${process.env.PORT || 9000}...`);
 });
